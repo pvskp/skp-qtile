582bc96162235aca39aec2fd6ce7430622d59523
pvskp <pvincius14@gmail.com>
2024-07-28 16:54:21 -0300

    chore: create utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import psutil
+import globals
+from libqtile.lazy import lazy
+from libqtile import qtile
 
 
 def get_current_wireless_interface() -> str:
@@ -8,3 +11,11 @@ def get_current_wireless_interface() -> str:
             if iface[0:2] == "wl":
                 return iface
     return ""
+
+
+def open_htop() -> None:
+    qtile.cmd_spawn(f"{globals.TERMINAL} -e htop")
+
+
+def open_nmtui() -> None:
+    qtile.cmd_spawn(f"{globals.TERMINAL} -e nmtui")
