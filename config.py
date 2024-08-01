8cf32a7020a4a8e068513e7db6445a4465fecd0d
pvskp <pvincius14@gmail.com>
2024-08-01 14:54:38 -0300

    chore: add new labels to groups

--- config.py
+++ config.py
@@ -108,8 +108,8 @@ for vt in range(1, 8):
 groups = []
 
 group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-group_labels = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-group_layouts = ["columns", "columns", "treetab", "columns", "columns", "columns", "columns", "columns", "columns"]
+group_labels = ["󰈹 ", " ", " ", " ", " ", " ", " ", "󱃾 ", " "]
+group_layouts = ["treetab", "columns", "treetab", "columns", "columns", "columns", "columns", "columns", "columns"]
 
 for i in range(len(group_names)):
     groups.append(
